95dc64ef85a685cf34dfea9ff28a71cfd67ad766
Waris Waheed <92310115+Waris69@users.noreply.github.com>
2025-12-23 13:57:33 +0500

    Add files via upload

---
+++ env.js
@@ -0,0 +1,9 @@
+export const ENV = {
+    apiKey: "AIzaSyCtgE80bKwp3ZqeKFzhoBX_cpzw1MZHzV0",
+    authDomain: "cartons-management.firebaseapp.com",
+    projectId: "cartons-management",
+    storageBucket: "cartons-management.firebasestorage.app",
+    messagingSenderId: "153740832676",
+    appId: "1:153740832676:web:10da33b94a1077476e9b31",
+    measurementId: "G-LS0KFJSTZX"
+};
